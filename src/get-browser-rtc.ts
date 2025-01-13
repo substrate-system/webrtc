530f5394935d0620cec43f9eead97a123e15b573
nichoth <nichoth@nichoth.com>
2025-01-12 19:58:36 -0800

    wip

---
+++ src/get-browser-rtc.ts
@@ -0,0 +1,34 @@
+export type BrowserRtc = {
+    RTCPeerConnection:typeof RTCPeerConnection;
+    RTCSessionDescription:typeof RTCSessionDescription;
+    RTCIceCandidate:typeof RTCIceCandidate;
+}
+
+/**
+ * Deal with vendor prefixes.
+ */
+export function getBrowserRTC ():BrowserRtc|null {
+    if (typeof globalThis === 'undefined') return null
+
+    const wrtc = {
+        RTCPeerConnection: (
+            globalThis.RTCPeerConnection ||
+            globalThis.mozRTCPeerConnection ||
+            globalThis.webkitRTCPeerConnection
+        ),
+        RTCSessionDescription: (
+            globalThis.RTCSessionDescription ||
+            globalThis.mozRTCSessionDescription ||
+            globalThis.webkitRTCSessionDescription
+        ),
+        RTCIceCandidate: (
+            globalThis.RTCIceCandidate ||
+            globalThis.mozRTCIceCandidate ||
+            globalThis.webkitRTCIceCandidate
+        )
+    }
+
+    if (!wrtc.RTCPeerConnection) return null
+
+    return wrtc
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,45 @@
 import { createDebug } from '@substrate-system/debug'
+import { type BrowserRtc, getBrowserRTC } from './get-browser-rtc'
 const debug = createDebug()
 
-export function example ():void {
-    debug('hello')
+type PeerOpts = {
+    config:RTCConfiguration;
+    allowHalfOpen:boolean;
+    id:string;
+    channelName:string;
+    initiator:boolean;
+    channelConfig:any;
+    channelNegotiated;
+    offerOptions:any;
+    answerOptions:any;
+    sdpTransform:((arg)=>typeof arg)
+}
+
+// const config: RTCConfiguration = {
+//     iceServers: [
+//         {
+//             urls: 'stun:stun.l.google.com:19302'
+//         }
+//     ],
+//     iceTransportPolicy: 'all',
+//     bundlePolicy: 'balanced',
+//     rtcpMuxPolicy: 'require'
+// };
+
+export class Peer {
+    private _wrtc:BrowserRtc
+    private _peerConnection:RTCPeerConnection
+    config?:RTCConfiguration
+
+    constructor (opts:Partial<PeerOpts>) {
+        debug('constructing', opts)
+
+        const rtc = getBrowserRTC()
+        if (!rtc) throw new Error('RTC does not exist')
+        this._wrtc = rtc
+        this.config = opts.config
+        this._peerConnection = new this._wrtc.RTCPeerConnection(
+            this.config,
+        )
+    }
 }

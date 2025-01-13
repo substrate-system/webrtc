21bc9e713d0a36df7cd4d637e90240ad69661d15
nichoth <nichoth@nichoth.com>
2025-01-12 23:10:56 -0800

    wip

--- example/index.ts
+++ example/index.ts
@@ -2,6 +2,7 @@ import { type FunctionComponent, render } from 'preact'
 import { useCallback } from 'preact/hooks'
 import { html } from 'htm/preact'
 import { TextInput } from '@nichoth/components/htm/text-input'
+import { computed, signal } from '@preact/signals'
 import { Peer } from '../src/index.js'
 import '@nichoth/components/text-input.css'
 import Debug from '@substrate-system/debug'
@@ -11,35 +12,89 @@ const peer = new Peer()
 
 debug('the peer', peer)
 
+const state = {
+    status: signal<'disconnected'|'connected'>('disconnected')
+}
+
+const isConnected = computed(() => {
+    return state.status.value === 'connected'
+})
+
+// @ts-expect-error dev
+window.peer = peer
+
+peer.on('open', () => {
+    state.status.value = 'connected'
+})
+
+peer.on('close', () => {
+    state.status.value = 'disconnected'
+})
+
 const Example:FunctionComponent<unknown> = function () {
-    const click = useCallback((ev:MouseEvent) => {
+    const connect = useCallback((ev:MouseEvent) => {
         ev.preventDefault()
-        debug('click')
+        debug('connecting...')
+        peer.connect()
     }, [])
 
-    const submit = useCallback((ev:SubmitEvent) => {
+    const listen = useCallback((ev:MouseEvent) => {
+        ev.preventDefault()
+        debug('listening...')
+        peer.listen()
+    }, [])
+
+    const sendMsg = useCallback((ev:SubmitEvent) => {
         ev.preventDefault()
         const form = ev.target as HTMLFormElement
         const input = form.elements['text'].value
-        debug('submit the text', input)
+        debug('send the text', input)
+        peer.channel?.send(input)
     }, [])
 
     return html`<div>
-        <button onClick=${click}>connect</button>
+        <div>
+            <span class="status ${state.status.value}">
+                <strong>status: </strong>
+                <span>${state.status.value}</span>
+            </span>
+        </div>
+
+        <div class="controls">
+            <button onClick=${connect} disabled=${isConnected}>
+                connect
+            </button>
+            <button onClick=${listen}>listen</button>
+        </div>
 
         <hr />
 
-        <form onSubmit=${submit}>
+        <form onSubmit=${sendMsg}>
             <${TextInput}
                 className="input"
                 displayName="message"
                 class="message"
                 name="text"
             />
-            <button type="submit">send</button>
+
+            <button
+                disabled=${!isConnected.value}
+                type="submit"
+            >
+                send
+            </button>
         </form>
         
     </div>`
 }
 
-render(html`<${Example} />`, document.getElementById('root')!)
+// const localConnection = null  // RTCPeerConnection for our "local" connection
+// const remoteConnection = null  // RTCPeerConnection for the "remote"
+
+// const sendChannel = null  // RTCDataChannel for the local (sender)
+// const receiveChannel = null  // RTCDataChannel for the remote (receiver)
+
+window.addEventListener('load', () => {
+    render(html`<${Example} />`, document.getElementById('root')!)
+}, false)
+
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { createDebug } from '@substrate-system/debug'
+import { createNanoEvents } from 'nanoevents'
 import { type BrowserRtc, getBrowserRTC } from './get-browser-rtc'
 const debug = createDebug()
 
@@ -26,14 +27,20 @@ type PeerOpts = {
 //     rtcpMuxPolicy: 'require'
 // };
 
+interface Events {
+    connect:()=>void;
+    open:()=>void;
+    close:()=>void;
+}
+
 export class Peer {
     private _wrtc:BrowserRtc
     private _pc:RTCPeerConnection
+    private _emitter:ReturnType<typeof createNanoEvents<Events>>
+    channel?:RTCDataChannel
     config?:RTCConfiguration
 
     constructor (opts:Partial<PeerOpts> = {}) {
-        debug('constructing', opts)
-
         const rtc = getBrowserRTC()
         if (!rtc) throw new Error('RTC does not exist')
         this._wrtc = rtc
@@ -41,17 +48,64 @@ export class Peer {
         this._pc = new this._wrtc.RTCPeerConnection(
             this.config,
         )
+
+        this._emitter = createNanoEvents<Events>()
+    }
+
+    on<E extends keyof Events> (ev:E, cb:Events[E]) {
+        return this._emitter.on(ev, cb)
+    }
+
+    handleSendChannelStatusChange (ev) {
+        if (!this.channel) return  // for TS
+        const channel = this.channel
+        const state = channel.readyState
+
+        if (state === 'open') {
+            debug('its open!', ev)
+            this._emitter.emit('open')
+        } else {
+            debug('closed!', ev)
+            this._emitter.emit('close')
+        }
     }
 
     connect () {
-        const channel = this._pc.createDataChannel('abc')
+        this.channel = this._pc.createDataChannel('abc')
+        const channel = this.channel!
+
         channel.onopen = (ev) => {
             debug('got "open" event', ev)
+            this.handleSendChannelStatusChange(ev)
             channel.send('Hello')
         }
 
         channel.onmessage = (ev) => {
             debug('got a message', ev.data)
+        }
+
+        channel.onclose = ev => {
+            debug('channel closed', ev)
+            this.handleSendChannelStatusChange(ev)
+        }
+
+        debug('the channel', channel)
+    }
+
+    listen () {
+        this._pc.ondatachannel = (ev:RTCDataChannelEvent) => {
+            const receiveChannel = ev.channel
+            receiveChannel.onmessage = ev => {
+                debug('got a message in listener', ev)
+            }
+
+            receiveChannel.onopen = ev => {
+                debug('channel opened...', ev)
+            }
+
+            receiveChannel.onclose = (ev) => {
+                debug('channel closed...', ev)
+            }
         }
     }
 }

a8da2f2b325219415d088327be731e4856906a54
nichoth <nichoth@nichoth.com>
2025-09-28 13:19:29 -0700

    wip tests

--- example_backend/party-server.ts
+++ example_backend/party-server.ts
@@ -2,12 +2,14 @@ import type * as Party from 'partykit/server'
 import Server, { defaultHeaders } from '../src/server.js'
 
 export default class PartyServer extends Server {
-    /**
-     * Example of setting headers on the response.
-     */
     async onRequest (req:Party.Request):Promise<Response> {
+        if (req.method === 'HEAD') {  // for `wait-on` CLI tool
+            return new Response(null, { status: 200 })
+        }
+
         const res = await super.onRequest(req)
 
+        // example: set the headers in response
         return new Response(res.body, {
             status: res.status,
             statusText: res.statusText,
--- test/browser/index.ts
+++ test/browser/index.ts
@@ -1,6 +1,10 @@
-import 'dotenv/config'
 import { test } from '@substrate-system/tapzero'
 
 test('example', async t => {
     t.ok(true, 'should be an example')
+})
+
+test('all done', () => {
+    // @ts-expect-error tests
+    window.testsFinished = true
 })

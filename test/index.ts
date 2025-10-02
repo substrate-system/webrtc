70b7080ad841232de19b0d4ab0b41fa4b9113db6
nichoth <nichoth@nichoth.com>
2025-10-02 12:05:40 -0700

    style

--- test/index.ts
+++ test/index.ts
@@ -1,17 +1,10 @@
 import 'dotenv/config'
 import { test } from '@substrate-system/tapzero'
 
-test('WebRTC module basic test', async t => {
-    // Since we're having issues with the complex Playwright setup,
-    // let's create a simpler test first to verify the basic functionality
-
-    console.log('Testing basic WebRTC module functionality...')
-
+test('WebRTC exports things', async t => {
     // Test that the module can be imported
     const { connect, Connection } = await import('../dist/index.js')
 
-    t.ok(connect, 'connect function should be exported')
-    t.ok(Connection, 'Connection class should be exported')
-
-    console.log('Basic WebRTC module test completed successfully!')
+    t.ok(connect, 'connect function should exist')
+    t.ok(Connection, 'Connection class should exist')
 })
